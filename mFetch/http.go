7e27f675f7939c68edd23727d76075b7a587a9b6
墨七 <meichangliang@mo7.cc>
2022-04-19 04:59:04 +0800

    addFetch

--- mFetch/http.go
+++ mFetch/http.go
@@ -9,6 +9,30 @@ import (
 	jsoniter "github.com/json-iterator/go"
 )
 
+/*
+
+	data := map[string]any{
+		"op": "subscribe",
+		"args": []string{
+			Channel, InstID, "mei",
+		},
+	}
+
+	res := mFetch.NewHttp(mFetch.HttpParam{
+		Origin: "http://mo7.cc:9000",
+		Path:   "/api/markets/tickers",
+		Data:   data,
+		Method: "get",
+		Header: map[string]string{
+			"Content-Type":  "appli1arset=utf-8",
+			"Content-Type1": "appl2et=utf-8",
+			"Content-Type2": "applicati3et=utf-8",
+			"Content-Type3": "application4t=utf-8",
+		},
+	})
+
+*/
+
 type HttpParam struct {
 	Origin string
 	Path   string
--- main.go
+++ main.go
@@ -12,26 +12,6 @@ func main() {
 	Channel := "candle1m"
 	InstID := "AVAX-USDT-mcl"
 
-	data := map[string]any{
-		"op": "subscribe",
-		"args": []string{
-			Channel, InstID, "mei",
-		},
-	}
-
-	res := mFetch.NewHttp(mFetch.HttpParam{
-		Origin: "http://mo7.cc:9000",
-		Path:   "/api/markets/tickers",
-		Data:   data,
-		Method: "get",
-		Header: map[string]string{
-			"Content-Type":  "appli1arset=utf-8",
-			"Content-Type1": "appl2et=utf-8",
-			"Content-Type2": "applicati3et=utf-8",
-			"Content-Type3": "application4t=utf-8",
-		},
-	})
-
 	fmt.Println(string(res))
 
 	fmt.Println(" =========   END   ========= ")
