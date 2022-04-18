4864adc8f265644ad62c7ef663a8e20f9bc21bf0
墨七 <meichangliang@mo7.cc>
2022-04-19 02:32:14 +0800

    exit-push

--- main.go
+++ main.go
@@ -2,36 +2,13 @@ package main
 
 import (
 	"fmt"
-	"time"
 
-	"github.com/EasyGolang/goTools/mCycle"
 	"github.com/EasyGolang/goTools/mTime"
 )
 
 func main() {
 	fmt.Println(" =========  START  ========= ")
-
-	in := 0
-
-	Ticker := func() {
-		Unix := mTime.GetUnix()
-		time := mTime.MsToTime(Unix, "0")
-		in++
-		fmt.Println("Ticker", time, in)
-	}
-
-	t := mCycle.New(mCycle.CycleParam{
-		Func:      Ticker,
-		SleepTime: time.Second,
-	})
-
-	t.Start()
-
-	time.Sleep(time.Second * 10)
-
-	t.End()
-
-	time.Sleep(time.Hour)
+	fmt.Println(mTime.GetUnix())
 
 	fmt.Println(" =========   END   ========= ")
 }
