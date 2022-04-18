c679b025249d04c49461fb40a2e76e872efed82b
墨七 <meichangliang@mo7.cc>
2022-04-19 02:34:20 +0800

    exit-push

--- mStart/Start.go
+++ mStart/Start.go
@@ -6,7 +6,7 @@ func Start() {
 	logStr := `
 	#########################################################
 	####                欢迎使用 goTools                   ###     
-	####                当前版本 v0.5.2                    ###     
+	####                当前版本 v0.5.3                    ###     
 	####              go clean --modcache                 ###     
 	####          作者邮箱 meichangliang@mo7.cc            ###      
 	#### 项目地址 https://github.com/EasyGolang/goTools ###     
