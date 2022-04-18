4cdbbf30e11f5432b05907092720e7ea3613b08f
墨七 <meichangliang@mo7.cc>
2022-04-19 02:38:21 +0800

    exit-push

--- mStart/Start.go
+++ mStart/Start.go
@@ -1,4 +1,4 @@
-package mTest
+package mStart
 
 import "fmt"
 
@@ -6,7 +6,7 @@ func Start() {
 	logStr := `
 	#########################################################
 	####                欢迎使用 goTools                   ###     
-	####                当前版本 v0.5.3                    ###     
+	####                当前版本 v0.5.4                    ###     
 	####              go clean --modcache                 ###     
 	####          作者邮箱 meichangliang@mo7.cc            ###      
 	#### 项目地址 https://github.com/EasyGolang/goTools ###     
--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/EasyGolang/goTools/mTime"
 )
 
 func main() {
 	fmt.Println(" =========  START  ========= ")
-	fmt.Println(mTime.GetUnix())
+	fmt.Println(mStart.Start())
 
 	fmt.Println(" =========   END   ========= ")
 }
