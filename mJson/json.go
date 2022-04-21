d407dae73a15b9d092fbb2c2b9f4796c6e2d77c6
墨七 <meichangliang@mo7.cc>
2022-04-22 07:11:17 +0800

    exit-push

--- mJson/json.go
+++ mJson/json.go
@@ -1,11 +1,22 @@
 package mJson
 
-import jsoniter "github.com/json-iterator/go"
+import (
+	"github.com/EasyGolang/goTools/mStr"
+	jsoniter "github.com/json-iterator/go"
+)
 
 func ToJson(data any) []byte {
 	jsonStr, err := jsoniter.Marshal(data)
 	if err != nil {
 		return []byte{}
 	}
+	return jsonStr
+}
+
+func ToStr(data any) string {
+	byteStr := ToJson(data)
+
+	jsonStr := mStr.ToStr(byteStr)
+
 	return jsonStr
 }
