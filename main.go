7b474155ee223982f08954debc093d24e21539cf
墨七 <meichangliang@mo7.cc>
2022-05-31 00:55:24 +0800

    exit-push

--- main.go
+++ main.go
@@ -3,18 +3,10 @@ package main
 import (
 	_ "embed"
 	"fmt"
-
-	"github.com/EasyGolang/goTools/mTikker"
 )
 
 func main() {
 	fmt.Println(" =========  START  ========= ")
-
-	mTikker.NewTikker(mTikker.TikkerOpt{
-		ShellContent: `
-		mEcho "我爱你22"
-		`,
-	}).RunToPm2()
 
 	fmt.Println(" =========   END   ========= ")
 }
