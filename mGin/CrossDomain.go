e7d0674bce208ccde417ee6f10da0a626b287cec
墨七 <meichangliang@mo7.cc>
2022-06-06 01:51:44 +0800

    exit-push

--- mGin/CrossDomain.go
+++ mGin/CrossDomain.go
@@ -6,28 +6,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func CrossDomain() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// 请求方法
-		method := c.Request.Method
-		// 指明哪些请求源被允许访问资源，值可以为 "*"（允许访问所有域），"null"，或者单个源地址。
-		c.Header("Access-Control-Allow-Origin", "*")
-		// 对于预请求来说，哪些请求方式可以用于实际的请求。
-		c.Header("Access-Control-Allow-Methods", "*")
-		// 对于预请求来说，指明哪些头信息可以安全的暴露给 CORS API 规范的 API
-		c.Header("Access-Control-Expose-Headers", "*")
-		// 允许Header
-		c.Header("Access-Control-Allow-Headers", "*")
-		// 允许客户端传递校验信息比如 cookie (重要)
-		c.Header("Access-Control-Allow-Credentials", "true")
+func CrossDomain(c *gin.Context) {
+	// 请求方法
+	method := c.Request.Method
+	// 指明哪些请求源被允许访问资源，值可以为 "*"（允许访问所有域），"null"，或者单个源地址。
+	c.Header("Access-Control-Allow-Origin", "*")
+	// 对于预请求来说，哪些请求方式可以用于实际的请求。
+	c.Header("Access-Control-Allow-Methods", "*")
+	// 对于预请求来说，指明哪些头信息可以安全的暴露给 CORS API 规范的 API
+	c.Header("Access-Control-Expose-Headers", "*")
+	// 允许Header
+	c.Header("Access-Control-Allow-Headers", "*")
+	// 允许客户端传递校验信息比如 cookie (重要)
+	c.Header("Access-Control-Allow-Credentials", "true")
 
-		// 设置返回格式是json
-		c.Set("Content-Type", "application/json; charset=utf-8")
-		// 放行所有OPTIONS方法
-		if method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusNoContent)
-		}
-		// 处理请求
-		c.Next()
+	// 设置返回格式是json
+	c.Set("Content-Type", "application/json; charset=utf-8")
+	// 放行所有OPTIONS方法
+	if method == "OPTIONS" {
+		c.AbortWithStatus(http.StatusNoContent)
 	}
 }
