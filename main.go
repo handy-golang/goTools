83496325419ec356ba2a05335252ffcfcc2549ca
墨七 <meichangliang@mo7.cc>
2022-06-11 13:37:50 +0800

    exit-push

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	_ "embed"
 	"fmt"
+
+	"github.com/EasyGolang/goTools/mEncrypt"
 )
 
 func main() {
 	fmt.Println(" =========  START  ========= ")
+
+	fmt.Println(mEncrypt.TimeID())
 
 	fmt.Println(" =========   END   ========= ")
 }
