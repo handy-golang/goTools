e7ee6afccbfd84bbe8c5326a3ce37dd2787171bc
墨七 <meichangliang@mo7.cc>
2022-07-20 00:23:50 +0800

    exit-push

--- mFetch/http.go
+++ mFetch/http.go
@@ -9,6 +9,34 @@ import (
 	jsoniter "github.com/json-iterator/go"
 )
 
+/*
+
+	data := map[string]any{
+		"op": "subscribe",
+		"args": []string{
+			"123", "345", "mei",
+		},
+	}
+
+	resData, err := mFetch.NewHttp(mFetch.HttpOpt{
+		Origin: "http://localhost:9000",
+		Path:   "/api/ping",
+		Data:   data,
+		Header: map[string]string{
+			"Content-Type":  "appli1arset=utf-8",
+			"Content-Type1": "appl2et=utf-8",
+			"Content-Type2": "applicati3et=utf-8",
+			"Content-Type3": "application4t=utf-8",
+		},
+	}).Get()
+	if err != nil {
+		fmt.Println("err", err)
+	}
+	jsonStr := mJson.JsonFormat(resData)
+	fmt.Println("resData", jsonStr)
+
+*/
+
 type HttpOpt struct {
 	Origin string
 	Path   string
