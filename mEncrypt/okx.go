d36846b0e24fdafc5913a6133cb6325baf34ae43
墨七 <meichangliang@mo7.cc>
2022-07-22 01:26:16 +0800

    exit-push

--- mEncrypt/okx.go
+++
@@ -1,7 +0,0 @@
-package mEncrypt
-
-import "strings"
-
-func OKXHashString(timestamp string, method string, requestPath string, body string) string {
-	return timestamp + strings.ToUpper(method) + requestPath + body
-}
