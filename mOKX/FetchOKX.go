f6b2f41a47b9a1a07d7aa737123e56c568585039
墨七 <meichangliang@mo7.cc>
2022-08-03 01:53:52 +0800

    exit-push

---
+++ mOKX/FetchOKX.go
@@ -0,0 +1,44 @@
+package mOKX
+
+import (
+	"io/ioutil"
+	"strings"
+
+	"github.com/EasyGolang/goTools/mFetch"
+)
+
+type OptFetchOKX struct {
+	Path          string
+	Data          map[string]any
+	LocalJsonData string // 本地的数据源
+	Method        string
+	IsLocalData   bool
+	Event         func(string, any)
+}
+
+func FetchOKX(opt OptFetchOKX) (resData []byte, resErr error) {
+	// 本地模式
+	if opt.IsLocalData {
+		return ioutil.ReadFile(opt.LocalJsonData)
+	}
+
+	if len(opt.Method) < 1 {
+		opt.Method = "GET"
+	}
+
+	// 处理 Header 和 加密信息
+	Method := strings.ToUpper(opt.Method)
+
+	fetch := mFetch.NewHttp(mFetch.HttpOpt{
+		Origin: "https://www.okx.com",
+		Path:   opt.Path,
+		Data:   opt.Data,
+		Event:  opt.Event,
+	})
+
+	if Method == "GET" {
+		return fetch.Get()
+	} else {
+		return fetch.Post()
+	}
+}
