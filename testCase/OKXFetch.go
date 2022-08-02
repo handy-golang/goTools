01db5a6ed4da68abbebc95b8b02470f37fffc0af
墨七 <meichangliang@mo7.cc>
2022-08-03 03:09:47 +0800

    exit-push

--- testCase/OKXFetch.go
+++ testCase/OKXFetch.go
@@ -19,6 +19,11 @@ func OKXFetch() {
 		Event: func(s string, a any) {
 			global.Log.Println("Event", s, a)
 		},
+		OKXKey: mOKX.TypeOkxKey{
+			ApiKey:     "ca6e399d-cc78-41b0-90fe-40ab99fb8040",
+			SecretKey:  "EF88B67D7E618FF6B854359BE88A6445",
+			Passphrase: "@Mcl931750",
+		},
 	})
 	if err != nil {
 		fmt.Println(err)
