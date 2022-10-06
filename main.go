e272f517a80c20c5534e21bde959effde659b7ee
墨七 <meichangliang@mo7.cc>
2022-10-07 07:25:53 +0800

    exit-push

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/EasyGolang/goTools/global"
 	"github.com/EasyGolang/goTools/global/config"
-	"github.com/EasyGolang/goTools/testCase"
 	jsoniter "github.com/json-iterator/go"
 )
 
@@ -33,6 +32,6 @@ func main() {
 
 	// testCase.FileTest()
 
-	testCase.YaSuoDir()
+	// testCase.YaSuoDir()
 	fmt.Println(" =========   END   ========= ")
 }
