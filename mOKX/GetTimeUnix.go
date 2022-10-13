e7c826dddcaed90cef6fba8ba19c6fd0b8249e8c
墨七 <meichangliang@mo7.cc>
2022-10-14 07:22:28 +0800

    exit-push

--- mOKX/GetTimeUnix.go
+++ mOKX/GetTimeUnix.go
@@ -4,14 +4,16 @@ func GetKdataTime(TickerKdata map[string][]TypeKd) int64 {
 	keys := []string{}
 
 	for key, val := range TickerKdata {
-		if len(val) > 0 {
+		if len(val) > 1 {
 			keys = append(keys, key)
 		}
 	}
 	var TimeUnix int64
 
 	if len(keys) > 0 {
-		TimeUnix = TickerKdata[keys[0]][0].TimeUnix
+		key := keys[len(keys)-1]
+		KdataList := TickerKdata[key]
+		TimeUnix = KdataList[len(KdataList)-1].TimeUnix
 	}
 
 	return TimeUnix
