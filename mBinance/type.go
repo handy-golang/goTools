2fab6fe74cace22fe4f7cd45e950805cc4a4331d
墨七aws <mo7@mo7.cc>
2022-12-05 17:16:03 +0800

    exit-push

--- mBinance/type.go
+++ mBinance/type.go
@@ -1,10 +1,11 @@
 package mBinance
 
 type PositionType struct {
-	PositionID    string `bson:"PositionID"`
 	InstID        string `bson:"InstID"`
 	Dir           int    `bson:"Dir"`
 	Profit        string `bson:"Profit"` // 持仓数量
 	CreateTime    int64  `bson:"CreateTime"`
 	CreateTimeStr string `bson:"CreateTimeStr"`
+	UpdateTime    int64  `bson:"UpdateTime"`
+	UpdateTimeStr int64  `bson:"UpdateTimeStr"`
 }
