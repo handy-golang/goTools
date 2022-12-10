a4723088cd59219fd2305ba1b4931b93219e0898
墨七aws <mo7@mo7.cc>
2022-12-10 19:05:57 +0800

    exit-push

--- mOKX/Opt.go
+++ mOKX/Opt.go
@@ -9,49 +9,49 @@ import (
 type KdataBarType struct {
 	Interval int64  // 每一条数据之间间隔的毫秒数
 	Okx      string // Okx 的值
-	Bnb      string // Bnb 的值
+	Binance  string // Bnb 的值
 }
 
 var KdataBarOpt = map[string]KdataBarType{
 	"1m": {
 		Interval: mTime.UnixTimeInt64.Minute * 1,
 		Okx:      "1m",
-		Bnb:      "1m",
+		Binance:  "1m",
 	},
 	"3m": {
 		Interval: mTime.UnixTimeInt64.Minute * 3,
 		Okx:      "3m",
-		Bnb:      "3m",
+		Binance:  "3m",
 	},
 	"5m": {
 		Interval: mTime.UnixTimeInt64.Minute * 5,
 		Okx:      "5m",
-		Bnb:      "5m",
+		Binance:  "5m",
 	},
 	"15m": {
 		Interval: mTime.UnixTimeInt64.Minute * 15,
 		Okx:      "15m",
-		Bnb:      "15m",
+		Binance:  "15m",
 	},
 	"30m": {
 		Interval: mTime.UnixTimeInt64.Minute * 30,
 		Okx:      "30m",
-		Bnb:      "30m",
+		Binance:  "30m",
 	},
 	"1h": {
 		Interval: mTime.UnixTimeInt64.Hour * 1,
 		Okx:      "1H",
-		Bnb:      "1h",
+		Binance:  "1h",
 	},
 	"2h": {
 		Interval: mTime.UnixTimeInt64.Hour * 2,
 		Okx:      "2H",
-		Bnb:      "2h",
+		Binance:  "2h",
 	},
 	"4h": {
 		Interval: mTime.UnixTimeInt64.Hour * 4,
 		Okx:      "4H",
-		Bnb:      "4h",
+		Binance:  "4h",
 	},
 }
 
