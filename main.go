0186009771de1f3e89708b66d239dff79b01500c
墨七 <mo7@mo7.cc>
2023-01-26 01:52:25 +0800

    exit-push

--- main.go
+++ main.go
@@ -5,14 +5,16 @@ import (
 	"fmt"
 
 	"github.com/EasyGolang/goTools/global"
+	"github.com/EasyGolang/goTools/global/config"
 	"github.com/EasyGolang/goTools/testCase"
+	jsoniter "github.com/json-iterator/go"
 )
 
 //go:embed package.json
 var AppPackage []byte
 
 func main() {
-	// jsoniter.Unmarshal(AppPackage, &config.AppInfo)
+	jsoniter.Unmarshal(AppPackage, &config.AppInfo)
 	global.Start()
 
 	fmt.Println(" =========  START  ========= ")
