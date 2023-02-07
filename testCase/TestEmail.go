a17a5dc584e40319b11dca0882676def6dac53cc
墨七 <mo7@mo7.cc>
2023-02-07 23:58:53 +0800

    exit-push

--- testCase/TestEmail.go
+++ testCase/TestEmail.go
@@ -11,16 +11,27 @@ import (
 func TestEmail() {
 	TStr := "测试邮件" + mEncrypt.GetUUID()
 
+	fmt.Println("开始发送邮件")
+
 	err := mEmail.New(mEmail.Opt{
-		Account:  "trade@mo7.cc",
-		Password: "svaeJgMraNmdVFJu",
+		// Account:  "trade@mo7.cc",
+		// Password: "svaeJgMraNmdVFJu",
+		// Port:     "587",
+		// Host:     "smtp.exmail.qq.com",
+
+		Account:  "670188307@qq.com",
+		Password: "momwdnjstnuibcej",
+		Port:     "587",
+		Host:     "smtp.qq.com",
+
 		To: []string{
 			"meichangliang@outlook.com",
+			"670188307@qq.com",
+			"mo7@mo7.cc",
 		},
-		From:        "Hunter 测试 服务",
-		Subject:     "这里是Subject",
-		Port:        "587",
-		Host:        "smtp.exmail.qq.com",
+		From:    "Hunter 测试 服务",
+		Subject: "这里是Subject",
+
 		TemplateStr: TStr,
 		SendData: struct {
 			SysTime string
