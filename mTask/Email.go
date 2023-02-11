39ab2dcffe8d7d72a7e08b8ad98cf0f7452f9ffb
墨七 <mo7@mo7.cc>
2023-02-11 19:35:59 +0800

    exit-push

--- mTask/Email.go
+++ mTask/Email.go
@@ -6,12 +6,12 @@ type SysEmail struct {
 	To       []string `bson:"To"`      // 发给谁
 	Subject  string   `bson:"Subject"` // 标题是啥
 	SendData struct {
-		Title        string // 标题
-		Message      string // 消息
-		Content      string // 内容
-		SysTime      string // 系统时间
-		Source       string // 来源
-		SecurityCode string // 安全码
+		Title        string `bson:"Title"`        // 标题
+		Message      string `bson:"Message"`      // 消息
+		Content      string `bson:"Content"`      // 内容
+		SysTime      string `bson:"SysTime"`      // 系统时间
+		Source       string `bson:"Source"`       // 来源
+		SecurityCode string `bson:"SecurityCode"` // 安全码
 	} `bson:"SendData"` // 要发送邮件的数据内容
 }
 
@@ -20,12 +20,12 @@ type CodeEmail struct {
 	To       []string `bson:"To"`      // 发给谁
 	Subject  string   `bson:"Subject"` // 标题是啥
 	SendData struct {
-		VerifyCode   string
-		Action       string
-		Minute       string // 分钟数
-		SysTime      string // 系统时间
-		Source       string // 来源
-		SecurityCode string // 安全码
+		VerifyCode   string `bson:"VerifyCode"`
+		Action       string `bson:"Action"`
+		Minute       string `bson:"Minute"`       // 分钟数
+		SysTime      string `bson:"SysTime"`      // 系统时间
+		Source       string `bson:"Source"`       // 来源
+		SecurityCode string `bson:"SecurityCode"` // 安全码
 	} `bson:"SendData"` // 要发送邮件的数据内容
 }
 
@@ -34,9 +34,9 @@ type RegisterEmail struct {
 	To       []string `bson:"To"`      // 发给谁
 	Subject  string   `bson:"Subject"` // 标题是啥
 	SendData struct {
-		Password     string
-		SysTime      string // 系统时间
-		Source       string // 来源
-		SecurityCode string // 安全码
+		Password     string `bson:"Password"`
+		SysTime      string `bson:"SysTime"`      // 系统时间
+		Source       string `bson:"Source"`       // 来源
+		SecurityCode string `bson:"SecurityCode"` // 安全码
 	} `bson:"SendData"` // 要发送邮件的数据内容
 }
