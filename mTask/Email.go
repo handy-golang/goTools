70c60b8ab5a8d7498fbd3e80728bc42663fdff49
墨七 <mo7@mo7.cc>
2023-02-12 21:50:16 +0800

    “公共参数”

--- mTask/Email.go
+++ mTask/Email.go
@@ -5,49 +5,53 @@ import (
 	jsoniter "github.com/json-iterator/go"
 )
 
-// 系统邮件
-type SysEmailParam struct {
-	Title        string `bson:"Title"`        // 标题
-	Message      string `bson:"Message"`      // 消息
-	Content      string `bson:"Content"`      // 内容
+// ======= 公共参数 ================
+
+type EmailParamPublic struct {
 	SysTime      string `bson:"SysTime"`      // 系统时间
 	Source       string `bson:"Source"`       // 来源
 	SecurityCode string `bson:"SecurityCode"` // 安全码
 }
+
+type EmailPublic struct {
+	From    string   `bson:"From"`    // 从哪里来
+	To      []string `bson:"To"`      // 发给谁
+	Subject string   `bson:"Subject"` // 标题是啥
+}
+
+// ======= 公共参数 ================
+
+// 系统邮件
+type SysEmailParam struct {
+	Title   string `bson:"Title"`   // 标题
+	Message string `bson:"Message"` // 消息
+	Content string `bson:"Content"` // 内容
+	EmailParamPublic
+}
 type SysEmail struct {
-	From     string        `bson:"From"`     // 从哪里来
-	To       []string      `bson:"To"`       // 发给谁
-	Subject  string        `bson:"Subject"`  // 标题是啥
+	EmailPublic
 	SendData SysEmailParam `bson:"SendData"` // 要发送邮件的数据内容
 }
 
 // 验证码邮件
 type CodeEmailParam struct {
-	VerifyCode   string `bson:"VerifyCode"`
-	Action       string `bson:"Action"`
-	Minute       string `bson:"Minute"`       // 分钟数
-	SysTime      string `bson:"SysTime"`      // 系统时间
-	Source       string `bson:"Source"`       // 来源
-	SecurityCode string `bson:"SecurityCode"` // 安全码
+	VerifyCode string `bson:"VerifyCode"` // 验证码
+	Action     string `bson:"Action"`     // 行为
+	Minute     string `bson:"Minute"`     // 分钟数
+	EmailParamPublic
 }
 type CodeEmail struct {
-	From     string         `bson:"From"`     // 从哪里来
-	To       []string       `bson:"To"`       // 发给谁
-	Subject  string         `bson:"Subject"`  // 标题是啥
+	EmailPublic
 	SendData CodeEmailParam `bson:"SendData"` // 要发送邮件的数据内容
 }
 
 // 注册成功邮件
 type RegisterParam struct {
-	Password     string `bson:"Password"`
-	SysTime      string `bson:"SysTime"`      // 系统时间
-	Source       string `bson:"Source"`       // 来源
-	SecurityCode string `bson:"SecurityCode"` // 安全码
+	Password string `bson:"Password"` // 密码
+	EmailParamPublic
 }
 type RegisterEmail struct {
-	From     string        `bson:"From"`     // 从哪里来
-	To       []string      `bson:"To"`       // 发给谁
-	Subject  string        `bson:"Subject"`  // 标题是啥
+	EmailPublic
 	SendData RegisterParam `bson:"SendData"` // 要发送邮件的数据内容
 }
 
