fe72702f06557c273656af10187328ad36bba716
墨七 <mo7@mo7.cc>
2023-02-12 22:13:57 +0800

    exit-push

--- mTask/Email.go
+++ mTask/Email.go
@@ -5,6 +5,14 @@ import (
 	jsoniter "github.com/json-iterator/go"
 )
 
+type SendEmail struct {
+	From     string   `bson:"From"`
+	To       []string `bson:"To"`
+	Subject  string   `bson:"Subject"`
+	TmplName string   `bson:"TmplName"` // 邮件模板的名字
+	SendData any      `bson:"SendData"` // 邮件模板需要的数据
+}
+
 // 系统邮件
 type SysEmailParam struct {
 	Title        string `bson:"Title"`   // 标题
@@ -13,12 +21,6 @@ type SysEmailParam struct {
 	SysTime      string `bson:"SysTime"`
 	Source       string `bson:"Source"`
 	SecurityCode string `bson:"SecurityCode"`
-}
-type SysEmail struct {
-	From     string        `bson:"From"`
-	To       []string      `bson:"To"`
-	Subject  string        `bson:"Subject"`
-	SendData SysEmailParam `bson:"SendData"` // 要发送邮件的数据内容
 }
 
 // 验证码邮件
@@ -30,12 +32,6 @@ type CodeEmailParam struct {
 	Source       string `bson:"Source"`
 	SecurityCode string `bson:"SecurityCode"`
 }
-type CodeEmail struct {
-	From     string         `bson:"From"`
-	To       []string       `bson:"To"`
-	Subject  string         `bson:"Subject"`
-	SendData CodeEmailParam `bson:"SendData"` // 要发送邮件的数据内容
-}
 
 // 注册成功邮件
 type RegisterParam struct {
@@ -44,15 +40,9 @@ type RegisterParam struct {
 	Source       string `bson:"Source"`
 	SecurityCode string `bson:"SecurityCode"`
 }
-type RegisterEmail struct {
-	From     string        `bson:"From"`
-	To       []string      `bson:"To"`
-	Subject  string        `bson:"Subject"`
-	SendData RegisterParam `bson:"SendData"` // 要发送邮件的数据内容
-}
 
-func ToMapData(val SysEmail) (resData map[string]any) {
-	// 转换结果
+// 结构转换
+func ToMapData(val SendEmail) (resData map[string]any) {
 	jsonStr := mJson.ToJson(val)
 	jsoniter.Unmarshal(jsonStr, &resData)
 	return
