55472e1dcb21474f0f23499d556011b43420a75d
墨七 <mo7@mo7.cc>
2023-02-17 17:41:27 +0800

    exit-push

--- mTask/Email.go
+++ mTask/Email.go
@@ -5,36 +5,32 @@ import (
 	jsoniter "github.com/json-iterator/go"
 )
 
-type SendEmail struct {
-	From     string   `bson:"From"`
-	To       []string `bson:"To"`
-	Subject  string   `bson:"Subject"`
-	TmplName string   `bson:"TmplName"` // 邮件模板的名字
-	SendData any      `bson:"SendData"` // 邮件模板需要的数据
-}
-
-// 系统邮件
-type SysEmailParam struct {
-	Title        string `bson:"Title"`   // 标题
-	Message      string `bson:"Message"` // 消息
-	Content      string `bson:"Content"` // 内容
-	SysTime      string `bson:"SysTime"`
-	Source       string `bson:"Source"`
-	SecurityCode string `bson:"SecurityCode"`
-}
-
 // 验证码邮件
-type CodeEmailParam struct {
-	VerifyCode   string `bson:"VerifyCode"` // 验证码
-	Action       string `bson:"Action"`     // 行为
-	SysTime      string `bson:"SysTime"`
-	Source       string `bson:"Source"`
-	SecurityCode string `bson:"SecurityCode"`
+type CodeEmail struct {
+	To         []string `bson:"To"`
+	VerifyCode string   `bson:"VerifyCode"` // 验证码
+	Action     string   `bson:"Action"`     // 行为
 }
 
 // 注册成功邮件
 type RegisterParam struct {
-	Password     string `bson:"Password"` // 密码
+	Password string `bson:"Password"` // 密码
+}
+
+// 邮件通用模板
+type SendEmail struct {
+	From     string     `bson:"From"`
+	To       []string   `bson:"To"`
+	Subject  string     `bson:"Subject"`
+	TmplName string     `bson:"TmplName"` // 邮件模板的名字
+	SendData EmailParam `bson:"SendData"` // 邮件模板需要的数据
+}
+
+// 邮件通用模板
+type EmailParam struct {
+	Title        string `bson:"Title"`   // 标题
+	Message      string `bson:"Message"` // 消息
+	Content      string `bson:"Content"` // 内容
 	SysTime      string `bson:"SysTime"`
 	Source       string `bson:"Source"`
 	SecurityCode string `bson:"SecurityCode"`
