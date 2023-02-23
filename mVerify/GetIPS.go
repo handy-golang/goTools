4b445b1becc4a81d60789c879320ebfcf8bb73d9
墨七 <mo7@mo7.cc>
2023-02-23 23:56:25 +0800

    爬虫

--- mVerify/GetIPS.go
+++ mVerify/GetIPS.go
@@ -1,8 +1,10 @@
 package mVerify
 
 import (
+	_ "embed"
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
@@ -15,43 +17,66 @@ type IPAddressType struct {
 	City     string
 }
 
-// 利用 https://whatismyipaddress.com/ip/36.44.232.38
-// 可能需要墙外才能访问
-
 func GetIPS(ips []string) {
 	// for _, val := range ips {
 	// }
 }
 
+//go:embed WhatIsMyIpHeader.yaml
+var WhatIsMyIpHeader string
+
+/*
+https://www.ipshudi.com/36.44.232.38.htm
+*/
 func GetIPaddress(ip string) (resData IPAddressType, resErr error) {
 	if !IsIP(ip) {
 		resErr = fmt.Errorf("ip地址不正确")
 		return
 	}
 
+	HeaderMap := FileToHeader(WhatIsMyIpHeader)
+
 	var collyData []byte
 	c := colly.NewCollector()
 	c.OnRequest(func(r *colly.Request) {
-		// 添加header头
+		for key, val := range HeaderMap {
+			r.Headers.Set(key, val)
+		}
 	})
 	c.OnResponse(func(r *colly.Response) {
 		collyData = r.Body
-		fmt.Println(2222)
 	})
 	c.OnError(func(r *colly.Response, errStr error) {
 		collyData = r.Body
 		resErr = errStr
-
-		fmt.Println(11111)
 	})
 
-	c.Visit("https://whatismyipaddress.com/ip/36.44.232.38")
+	// c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+	// 	if err := e.Request.Visit(e.Attr("href")); err != nil {
+	// 		log.Printf("visit err: %v", err)
+	// 	}
+	// })
+
+	c.Visit("https://www.ipshudi.com/36.44.232.38.htm")
 
 	if resErr != nil {
 		return
 	}
-
-	log.Println(string(collyData))
+	log.Println("请求的Body", resErr, string(collyData))
 
 	return
+}
+
+func FileToHeader(cont string) map[string]string {
+	strArr := strings.Split(cont, "\n")
+	HeaderMap := make(map[string]string)
+	for _, item := range strArr {
+		kvArr := strings.Split(item, ": ")
+		if len(kvArr) == 2 {
+			k := kvArr[0]
+			v := kvArr[1]
+			HeaderMap[k] = v
+		}
+	}
+	return HeaderMap
 }
--- testCase/IPTest.go
+++ testCase/IPTest.go
@@ -1,14 +1,10 @@
 package testCase
 
 import (
-	"log"
-
 	"github.com/EasyGolang/goTools/mVerify"
 )
 
 func IPTest() {
 	ip := "36.44.232.38"
-	resData, err := mVerify.GetIPaddress(ip)
-
-	log.Println(resData, err)
+	mVerify.GetIPaddress(ip)
 }
