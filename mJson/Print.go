137a788b97c466054fe40f30087a0cd038deff9d
墨七 <mo7@mo7.cc>
2023-02-23 14:40:38 +0800

    exit-push

--- mJson/Print.go
+++ mJson/Print.go
@@ -1,11 +1,13 @@
 package mJson
 
-import "fmt"
+import (
+	"log"
+)
 
 // struct 进行 json 格式 打印
 func Println(data any) string {
 	json := ToJson(data)
 	jsonStr := JsonFormat(json)
-	fmt.Println(jsonStr)
+	log.Println(jsonStr)
 	return jsonStr
 }
